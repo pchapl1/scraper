304ae14212b3f1a14e6f16fac6a376a0b5fcf9b5
Phil Chaplin <pchapl1@gmail.com>
2021-10-12 08:14:21 -0700

    added procfile and updated yaml to deploy to heroku

--- conftest.py
+++ conftest.py
@@ -1,5 +1,6 @@
 import pytest
 from scraper import *
+from database import *
 
 @pytest.fixture
 def page_request():
@@ -10,3 +11,9 @@ def page_request():
 def page_html(page_request):
     bs = BeautifulSoup(page_request.content, 'html.parser')
     return bs
+    
+@pytest.fixture
+def conn_to_db():
+    con = sqlite3.connect('scrapeymcscraperton.db')
+    cur = con.cursor()
+    return cur
